397169b8d191241a39151328605b31af5f8c08d1
mohammed Natsha <alnatsham@gmail.com>
2020-10-25 15:33:43 +0300

    admin img post,put and delete

--- server/src/routers/admin-router.js
+++ server/src/routers/admin-router.js
@@ -11,12 +11,47 @@ router.post('/img', (req, res) => {
     });
 });
 
-router.put('/img/:id', (req,res) => {
+router.post('/img/many', (req, res) => {
+    let data = [];
+
+    for(let i=0;i<req.body.images.length;i++)
+    {
+        data.push({
+            url: req.body.images[i].url,
+            tags: req.body.images[i].tags
+        })
+    }
+
+    model.insertMany(data, function (err) {
+        if (err) res.status(501).send();;
+        res.status(201).send();
+    });
     
-})
+});
+
+router.put('/img/:id', (req,res) => {
+    Model.findOne({ _id: req.params.id }, (err, doc) => {
+        if(err) {
+            res.status(400).send();
+        } else {
+            doc.url = req.body.url;
+            doc.tags = req.body.tags;// for dataset preperation
+            doc.save();
+            res.status(201).send();
+        }
+      });
+});
 
 router.delete('/img/:id', (req,res) => {
-
+    Model.deleteOne({ _id: req.params.id }, (err) => {
+        if(err) {
+            res.status(400).send();
+        } else {
+            res.status(201).send();
+        }
+    });// for dataset preperation
 });
+
+
 
 module.exports = router;
